2e5477e5445b59e27383bd200a4604e405d92867
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2022-06-12 00:56:49 +0100

    Edit (Design, Code) of chat_app

---
+++ lib/chat_message.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+const String _name = "Messaoud";
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  const ChatMessage({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              // ignore: todo
+              //TODO: Add a picture in this CircleAvatar
+              child: Text(_name[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subtitle1),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,26 +10,65 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textEC = TextEditingController();
-  void _handleSubmitted(String txt) {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmitted(String text) {
     _textEC.clear();
+    ChatMessage message = ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return IconTheme(
+        data: const IconThemeData(color: Colors.blue),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                child: TextField(
+                  decoration: const InputDecoration.collapsed(
+                      hintText: "Send a message"),
+                  controller: _textEC,
+                  onSubmitted: _handleSubmitted,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: IconButton(
+                  onPressed: () => _handleSubmitted(_textEC.text),
+                  icon: const Icon(Icons.send),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: TextField(
-              decoration:
-                  const InputDecoration.collapsed(hintText: "Send a message"),
-              controller: _textEC,
-              onSubmitted: _handleSubmitted,
-            ),
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
           ),
-        ],
-      ),
+        ),
+        const Divider(
+          height: 1.0,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        ),
+      ],
     );
   }
 }
