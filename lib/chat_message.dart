2cd2f3ef56da6570f38bb6fb42515735014a88f2
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2022-06-13 23:11:04 +0100

    Edit (Design, Code) of chat_app 1

--- lib/chat_message.dart
+++ lib/chat_message.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: use_key_in_widget_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const String _name = "Messaoud";
+AssetImage assetImage1 = const AssetImage('images/my_photo.jpg');
+AssetImage assetImage2 = const AssetImage('images/my_img.jpg');
 
 class ChatMessage extends StatelessWidget {
   final String text;
@@ -11,22 +14,35 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      margin: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(8.0),
+      // ignore: prefer_const_constructors
+      decoration: BoxDecoration(
+          /*border: Border.all(
+            color: Colors.blue, width: 2.0, style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(20),*/
+          color: Colors.cyan[100],
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0))),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             margin: const EdgeInsets.only(right: 16.0),
             child: CircleAvatar(
-              // ignore: todo
-              //TODO: Add a picture in this CircleAvatar
-              child: Text(_name[0]),
+              radius: 25.0,
+              backgroundImage: assetImage2,
             ),
           ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(_name, style: Theme.of(context).textTheme.subtitle1),
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
               Container(
                 margin: const EdgeInsets.only(top: 5.0),
                 child: Text(text),
@@ -35,6 +51,19 @@ class ChatMessage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class GetImageAsset extends StatelessWidget {
+  const GetImageAsset({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = const AssetImage('images/my_photo.jpg');
+    return CircleAvatar(
+      radius: 25.0,
+      backgroundImage: assetImage1,
     );
   }
 }
